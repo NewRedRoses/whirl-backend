672cd267622a1e0233c3b313d634a14d1f4b9ed4
Mark A. <mark001616@gmail.com>
2025-02-18 20:30:55 -0500

    Remove extra spaces

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,10 +2,8 @@ const { Router } = require("express");
 
 const userRouter = Router();
 
-const { getUserData } = require("../controllers/userController.js");
 const { getUserProfile } = require("../controllers/userController.js");
 
-userRouter.get("/", getUserData);
 userRouter.get("/", getUserProfile);
 
 module.exports = userRouter;
