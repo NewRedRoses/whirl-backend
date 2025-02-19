222b5783880fd54ee8ea050a4e0eb4eadb5df4da
Mark A. <mark001616@gmail.com>
2025-02-18 20:30:41 -0500

    Return user profile data on verified GET

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,20 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const jwt = require("jsonwebtoken");
 
-const getUserData = async (req, res) => {
+const { getUserProfileByUserId } = require("../prisma/db.js");
+
 const getUserProfile = (req, res) => {
   jwt.verify(req.cookies.jwt, process.env.SECRET, async (errors, authData) => {
-    if (errors) {
-      return res.sendStatus(500);
+    try {
+      if (errors) {
+        return res.sendStatus(401);
+      }
+      const profileData = await getUserProfileByUserId(authData.id);
+      res.json(profileData);
+    } catch (err) {
+      console.log(err);
     }
-    res.json(authData);
   });
 };
 
-module.exports = { getUserData };
 module.exports = { getUserProfile };
